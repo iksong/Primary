e24ec3d66b11b719b6ea9717cf3fa8c6f4b58b45
IK Song <lcdf17@gmail.com>
2021-12-17 12:53:21 -0600

    added package

---
+++ Primary.xcframework/ios-arm64/Primary.framework/Headers/Primary.h
@@ -0,0 +1,18 @@
+//
+//  Primary.h
+//  Primary
+//
+//  Created by IK on 12/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Primary.
+FOUNDATION_EXPORT double PrimaryVersionNumber;
+
+//! Project version string for Primary.
+FOUNDATION_EXPORT const unsigned char PrimaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Primary/PublicHeader.h>
+
+
